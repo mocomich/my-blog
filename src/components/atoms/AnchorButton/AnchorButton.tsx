42804c7fb67ccefb02c71dd14edc7876c05ed05d
mk <mk.eng1119@gmail.com>
2023-02-25 19:37:56 +0900

    update: hover style

--- src/components/atoms/AnchorButton/AnchorButton.tsx
+++ src/components/atoms/AnchorButton/AnchorButton.tsx
@@ -3,5 +3,5 @@ import { ComponentPropsWithoutRef } from "react";
 type Props = ComponentPropsWithoutRef<"a"> & { disabled?: boolean };
 
 export const AnchorButton: React.FC<Props> = ({ disabled, ...props }) => {
-  return <a {...props} aria-disabled={disabled} className="hover:text-asset-blue mb-2  border border-black py-1 px-2 font-semibold text-black transition-all delay-75 hover:border-transparent dark:border-white dark:text-white " role="button" />;
+  return <a {...props} aria-disabled={disabled} className="mb-2 border  border-black py-1 px-2 font-semibold text-black transition-all delay-75 hover:border-transparent hover:text-asset-blue dark:border-white dark:text-white dark:hover:bg-gray-700 " role="button" />;
 };
--- src/components/molecules/NavMenu/NavMenu.tsx
+++ src/components/molecules/NavMenu/NavMenu.tsx
@@ -25,7 +25,7 @@ export const NavMenu: React.FC<Props> = ({ isOpen }) => {
       <ul className="items-center justify-center md:flex md:space-x-6 md:space-y-0">
         {HEADER_PATHS.map((path) => (
           <li className="text-gray-600" key={path.title}>
-            <Link className="hover:text-asset-blue flex h-12 min-h-full w-full items-center rounded-md pl-2 font-semibold hover:bg-gray-200 dark:text-white dark:hover:bg-gray-300 md:px-2" href={path.href}>
+            <Link className="flex h-12 min-h-full w-full items-center rounded-md pl-2 font-semibold hover:bg-gray-200 hover:text-asset-blue dark:text-white dark:hover:bg-gray-700 md:px-2" href={path.href}>
               {path.title}
             </Link>
           </li>
