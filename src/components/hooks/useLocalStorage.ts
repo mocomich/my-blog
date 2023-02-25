8d9652789a30f5612317760599502630551027de
mk <mk.eng1119@gmail.com>
2023-02-25 18:26:29 +0900

    update: add returnType

--- src/components/hooks/useLocalStorage.ts
+++ src/components/hooks/useLocalStorage.ts
@@ -5,7 +5,9 @@ type Props<T> = {
   key: string;
 };
 
-export const usePersistState = <T>({ key, initialValue }: Props<T>) => {
+type Result<T> = readonly [T, (v: T) => void];
+
+export const usePersistState = <T>({ key, initialValue }: Props<T>): Result<T> => {
   const getItemFromStorage = <T>(key: string, defaultValue?: T) => {
     try {
       const val = JSON.parse(localStorage.getItem(key) + "");
