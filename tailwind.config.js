b1879086c9f15e5eb843cb1b33b2f14b36d12148
mk <mk.eng1119@gmail.com>
2023-02-25 19:30:34 +0900

    update: darkMode setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  darkMode:"class",
   plugins: [require("@tailwindcss/line-clamp"), require("@tailwindcss/typography")],
   theme: {
     extend: {
