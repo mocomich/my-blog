830b8aa14ed125326c5390858578d85eddcc383d
mk <mk.eng1119@gmail.com>
2023-02-26 14:26:51 +0900

    remove: locale

--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -1,6 +1,5 @@
-import { format } from 'date-fns'
-import ja from 'date-fns/locale/ja'
+import { format } from "date-fns";
 
 export const formatDateToYYYYMMDD = (date: string) => {
-  return format(new Date(date), 'yyyy-MM-dd', { locale: ja })
-}
+  return format(new Date(date), "yyyy-MM-dd");
+};
